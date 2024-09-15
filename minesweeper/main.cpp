4dab29e53803ec024b271a0cd2ad2951d78688e5
elmerheino <elmer.heino2@gmail.com>
2024-09-15 12:36:10 +0530

    Initial Commit

---
+++ minesweeper/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  minesweeper
+//
+//  Created by Elmer Heino on 15.9.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
